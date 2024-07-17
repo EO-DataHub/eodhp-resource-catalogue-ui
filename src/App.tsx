f50c342a017e920e6d03b335996fca9f1f9f617c
james-hinton <james.hinton@live.co.uk>
2024-07-17 13:29:54 +0100

    Add collapsable sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-
+import { useState } from "react";
 import MapViewer from "./pages/MapViewer";
 import DataCatalogue from "./pages/DataCatalogue";
 import React from "react";
 import FilterSidebar from "./components/FilterSidebar";
+import { TbLayoutSidebarRightCollapseFilled } from "react-icons/tb";
 
 const App: React.FC = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState(true);
   return (
     <>
       <div className="main">
-        <div className="left-sidebar-container">
-          <FilterSidebar />
+
+        <div className={`left-sidebar-container ${isFilterOpen ? "left-sidebar-container-open" : "left-sidebar-container-closed"}`}>
+          <FilterSidebar
+            isFilterOpen={isFilterOpen}
+            setIsFilterOpen={setIsFilterOpen}
+          />
         </div>
+        {
+          !isFilterOpen && (
+            <div className="filter-collapse-open"
+              onClick={() => setIsFilterOpen(!isFilterOpen)}
+            >
+              <TbLayoutSidebarRightCollapseFilled />
+            </div>
+          )
+        }
+
 
         <div className="main-content-container">
           <DataCatalogue />
--- src/components/FilterSidebar/index.tsx
+++ src/components/FilterSidebar/index.tsx
@@ -1,12 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import MultiSelectFilter from "./components/MultiSelectFilter";
 import TemporalFilter from "./components/TemporalFilter";
 import './styles.scss'
 import { FilterContext } from "@/context/FilterContext";
 import { FilterData } from "@/context/FilterContext/types";
+import { TbLayoutSidebarLeftCollapseFilled } from "react-icons/tb";
 
-
-const FilterSidebar: React.FC = (): React.ReactNode => {
+const FilterSidebar: React.FC = ({ isFilterOpen, setIsFilterOpen }): React.ReactNode => {
   const { state } = useContext(FilterContext);
   const { filterOptions: filterOptions } = state;
 
@@ -23,24 +23,32 @@ const FilterSidebar: React.FC = (): React.ReactNode => {
   }
 
   return (
-    <div className="filter-sidebar">
-      <div className="filter-header">
-        <h4>Filter By</h4>
+    <>
+      <div className={`filter-sidebar`}>
+        <div className="filter-header">
+          <h4>Filter By</h4>
+        </div>
+
+        <div className="filter-body">
+          {filterOptions?.map((filter: FilterData) => (
+            <div key={filter.id} className="filter">
+              {renderFilterComponent(filter)}
+            </div>
+          ))}
+        </div>
+
+        <div className="filter-footer">
+          <button>Reset</button>
+        </div>
+
       </div>
 
-      <div className="filter-body">
-        {filterOptions?.map((filter: FilterData) => (
-          <div key={filter.id} className="filter">
-            {renderFilterComponent(filter)}
-          </div>
-        ))}
+      <div className="filter-collapse-close"
+        onClick={() => setIsFilterOpen(!isFilterOpen)}
+      >
+        <TbLayoutSidebarLeftCollapseFilled />
       </div>
-
-      <div className="filter-footer">
-        <button>Reset</button>
-      </div>
-
-    </div>
+    </>
   );
 
 };
