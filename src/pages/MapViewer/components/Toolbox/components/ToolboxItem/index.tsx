cfaffcce2f40573b35e2c3d222f33813b32ca255
james-hinton <james.hinton@live.co.uk>
2024-08-01 14:28:31 +0100

    Add WMTS support from EODHP VS

--- src/pages/MapViewer/components/Toolbox/components/ToolboxItem/index.tsx
+++ src/pages/MapViewer/components/Toolbox/components/ToolboxItem/index.tsx
@@ -4,6 +4,7 @@ import { useMap } from 'react-leaflet';
 import L from 'leaflet';
 import { ToolboxItemProps } from './types';
 
+import './leaflet-wmts.js';
 const ToolboxItem = (ItemData: ToolboxItemProps) => {
   const { thumbnail, title, dataPoints } = ItemData;
 
@@ -51,9 +52,44 @@ const ToolboxItem = (ItemData: ToolboxItemProps) => {
     placeholderLayer.addTo(map);
   }
 
+  const handleAddWMTSLayer = () => {
+    const url = "https://test.eodatahub.org.uk/vs/cache/ows/wmts"
+
+    let options = {
+      tileMatrixSet: 'GoogleMapsCompatible',
+      layer: 'LST_COLLECTION__LST_VISUALIZATION',
+      time: '2024-06-18T13:22:03Z/2024-07-08T12:40:47Z',
+    };
+
+    // @ts-ignore
+    let wmtsLayer = new L.tileLayer.wmts(url, options);
+    wmtsLayer.addTo(map);
+  }
+
   return (
-    <div className="toolbox-item"
-      onMouseUp={handleAddPlaceholderLayer}
+
+    <div
+      className="toolbox-item"
+      onMouseUp={(e) => {
+        // For proof of functionality with polygons and WMTS layers. The following applies to all Toolbox items:
+        // - Shift + Click: Add placeholder polygon
+        // - Click: Add WMTS layer from EOX View Server
+        // - Ctrl + Click: Remove all GeoJSON and WMTS layers
+        if (e.shiftKey) {
+          handleAddPlaceholderLayer();
+        }
+        if (e.ctrlKey) {
+          map.eachLayer((layer) => {
+            // @ts-ignore
+            if (layer instanceof L.GeoJSON || layer instanceof L.TileLayer.WMTS) {
+              map.removeLayer(layer);
+            }
+          })
+        }
+        else {
+          handleAddWMTSLayer();
+        }
+      }}
     >
       <div className="toolbox-item__left">
         <img src={thumbnail} alt="thumbnail" />
