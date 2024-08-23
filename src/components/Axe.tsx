63bb4de7ee0081723fb6b62eab0b93f0a568cddf
Mark Small <mark.small@telespazio.com>
2024-08-23 22:48:04 +0100

    fix(accessibility): Use correct Env Var for VITE
    I forgot VITE uses a weird way to access Env Vars i.e. `import.meta.env`
    I have now updated the code so it uses this instead of the normal
    `process.env` syntax.

--- src/components/Axe.tsx
+++ src/components/Axe.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Axe = () => {
   // Check we are not in production, axe should only be used during development.
-  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  if (typeof window !== 'undefined' && import.meta.env.MODE === 'development') {
     Promise.all([import('@axe-core/react'), import('react-dom')]).then(([axe, ReactDOM]) =>
       axe.default(React, ReactDOM, 1000),
     );
