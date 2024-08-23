fb189882b57ce751f513215e708822a835e9a805
Mark Small <mark.small@telespazio.com>
2024-08-23 15:28:29 +0100

    fix(accessibility): Check we are in the development environment
    I don't think this matters much, but it probably makes it more explicit
    that we only want this to work in `development` mode as opposed to any
    other as this should only be used on the developers machine.
    
    IssueID EODHP-619

--- src/components/Axe.tsx
+++ src/components/Axe.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Axe = () => {
   // Check we are not in production, axe should only be used during development.
-  if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
     Promise.all([import('@axe-core/react'), import('react-dom')]).then(([axe, ReactDOM]) =>
       axe.default(React, ReactDOM, 1000),
     );
