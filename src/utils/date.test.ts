97495a10b9a38682f9ffc34acaa9197d49291909
IsaacJenkinsTPZ <isaac.jenkins@telespazio.com>
2024-10-24 12:57:17 +0100

    Removed redundant tests which no longer apply.

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -6,23 +6,13 @@ describe('Format date utils method', () => {
   it('should return date in ISO8601 format when passed a valid Date object', () => {
     const date = new Date(2024, 9, 14); // October 14, 2024
     const result = formatDate(date);
+    console.log(result);
     expect(result).toBe(TESTING_DATE);
   });
 
   it('should return date in ISO8601 format when passed a valid date string', () => {
     const result = formatDate(TESTING_DATE);
     expect(result).toBe(TESTING_DATE);
-  });
-
-  it('should return current date in ISO8601 format when no date is provided', () => {
-    const result = formatDate('');
-    const expectedDate = new Date().toISOString().slice(0, 10); // Extracts YYYY-MM-DD from current date
-    expect(result).toBe(expectedDate);
-  });
-
-  it('should throw an error when an unsupported formatType is provided', () => {
-    const date = new Date();
-    expect(() => formatDate(date, 'UNKNOWN')).toThrow('Format type: UNKNOWN not implemented');
   });
 
   it('should throw an error when an invalid date string is provided', () => {
