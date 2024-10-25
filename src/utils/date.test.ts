1b63d5ce7ca385f63e1bd4b30acd220d06aa63c3
Isaac Jenkins <isaac.jenkins@telespazio.com>
2024-10-25 13:55:16 +0100

    Removed console.log

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -6,7 +6,6 @@ describe('Format date utils method', () => {
   it('should return date in ISO8601 format when passed a valid Date object', () => {
     const date = new Date(2024, 9, 14); // October 14, 2024
     const result = formatDate(date);
-    console.log(result);
     expect(result).toBe(TESTING_DATE);
   });
 
