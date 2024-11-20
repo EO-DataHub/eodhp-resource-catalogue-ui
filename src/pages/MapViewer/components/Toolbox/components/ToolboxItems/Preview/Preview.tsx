e809800c2f31ecee32b18b8918b77b9a067e23ea
Isaac Jenkins <isaac.jenkins@telespazio.com>
2024-11-20 16:48:42 +0000

    Linting.

--- src/pages/MapViewer/components/Toolbox/components/ToolboxItems/Preview/Preview.tsx
+++ src/pages/MapViewer/components/Toolbox/components/ToolboxItems/Preview/Preview.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import './styles.scss';
 
 import STACLayer from 'ol-stac/layer/STAC';
