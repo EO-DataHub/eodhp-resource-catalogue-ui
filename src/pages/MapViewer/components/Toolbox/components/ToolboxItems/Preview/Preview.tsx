23817c221a876ccdce2f9afe454323bfe61135a3
Isaac Jenkins <isaac.jenkins@telespazio.com>
2024-11-25 15:46:06 +0000

    Type now detected and used in layer.
    Preview will not show unless type specificed. I added a method to try and detect the type and this seems to have fixed the issue.

--- src/pages/MapViewer/components/Toolbox/components/ToolboxItems/Preview/Preview.tsx
+++ src/pages/MapViewer/components/Toolbox/components/ToolboxItems/Preview/Preview.tsx
@@ -17,6 +17,25 @@ const Preview = ({ item }: PreviewProps) => {
   const layerRef = useRef(null);
 
   useEffect(() => {
+    const thumbnailAsset = item.assets.thumbnail;
+    if (thumbnailAsset && !thumbnailAsset.type) {
+      const href = thumbnailAsset.href;
+
+      const extension = href.split('.').pop().toLowerCase();
+      const mimeTypes = {
+        jpg: 'image/jpeg',
+        jpeg: 'image/jpeg',
+        png: 'image/png',
+        gif: 'image/gif',
+        tiff: 'image/tiff',
+        tif: 'image/tiff',
+        webp: 'image/webp',
+      };
+      // Preview will not show if type not explicitly set. Hopefully we should
+      // receive the type but if not, this seems to fix the issue in most cases
+      thumbnailAsset.type = mimeTypes[extension] || 'image/jpeg';
+    }
+
     const stacLayer = new STACLayer({ data: item, displayPreview: true });
     layerRef.current = stacLayer;
   }, [item]);
